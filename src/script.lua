8dc9a494e26ac97c76436b5296bbdc38af973503
Matteo Autieri <matteoautieri@live.it>
2017-01-12 00:09:24 +0100

    updated to my latest base mod

--- src/script.lua
+++ src/script.lua
@@ -9,17 +9,64 @@ WashAtConfig = {};
 WashAtConfig.name = "WashAtConfig";
 WashAtConfig.debug = false;
 
-function WashAtConfig:print(txt)
-    if WashAtConfig.debug then
-        print("[" .. self.name .. "] -> " .. txt);
+function WashAtConfig:print(txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9)
+    if self.debug then
+        local args = {txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9};
+        for i, v in ipairs(args) do
+            if v then
+                print("[" .. self.name .. "] -> " .. tostring(v));
+            end
+        end
     end
 end
 
+function WashAtConfig:initialize(missionInfo, missionDynamicInfo, loadingScreen)
+    self = WashAtConfig;
+    self:print("initialize()");
+end
+g_mpLoadingScreen.loadFunction = Utils.prependedFunction(g_mpLoadingScreen.loadFunction, WashAtConfig.initialize);
+
+function WashAtConfig:load(missionInfo, missionDynamicInfo, loadingScreen)
+    self = WashAtConfig;
+    self:print("load()");
+    g_currentMission.missionInfo.saveToXML = Utils.appendedFunction(g_currentMission.missionInfo.saveToXML, self.saveSavegame);
+end
+g_mpLoadingScreen.loadFunction = Utils.appendedFunction(g_mpLoadingScreen.loadFunction, WashAtConfig.load);
+
 function WashAtConfig:loadMap(name)
+    self:print(("loadMap(name:%s)"):format(name));
+    if self.debug then
+        addConsoleCommand("AAAWashAtConfigeTestCommand", "", "TestCommand", self);
+    end
+    self:loadSavegame();
     ChangeVehicleConfigEvent.run = Utils.prependedFunction(ChangeVehicleConfigEvent.run, WashAtConfig.changeVehicleConfigRun);
 end
 
+function WashAtConfig:loadMapFinished()
+    self = WashAtConfig;
+    self:print("loadMapFinished()");
+end
+
+function WashAtConfig:afterLoad()
+    self = WashAtConfig;
+    self:print("afterLoad");
+end
+
+function WashAtConfig:loadSavegame()
+    self:print("loadSavegame()");
+end
+
+function WashAtConfig:saveSavegame()
+    self = WashAtConfig;
+    self:print("saveSavegame()");
+end
+
 function WashAtConfig:deleteMap()
+    self:print("deleteMap()");
+end
+
+function WashAtConfig:TestCommand()
+    return "This is a test command";
 end
 
 function WashAtConfig:keyEvent(unicode, sym, modifier, isDown)
