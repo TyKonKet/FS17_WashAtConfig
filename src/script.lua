8a3dbc5d44307e8f404c295166fc8db759b53883
Matteo Autieri <matteoautieri@live.it>
2017-03-20 15:55:16 +0100

    fee of 250 in workbench if you haven't washers

--- src/script.lua
+++ src/script.lua
@@ -1,13 +1,12 @@
 --
--- WashAtConfig script
---
+-- WashAtConfig
 --
 -- @author  TyKonKet
 -- @date 10/01/2017
-
 WashAtConfig = {};
 WashAtConfig.name = "WashAtConfig";
-WashAtConfig.debug = false;
+WashAtConfig.debug = true;
+WashAtConfig.washers = 0;
 
 function WashAtConfig:print(txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9)
     if self.debug then
@@ -24,6 +23,7 @@ function WashAtConfig:initialize(missionInfo, missionDynamicInfo, loadingScreen)
     self = WashAtConfig;
     self:print("initialize()");
     ChangeVehicleConfigEvent.run = Utils.prependedFunction(ChangeVehicleConfigEvent.run, WashAtConfig.changeVehicleConfigRun);
+    g_shopConfigScreen.setStoreItem = WashAtConfig.setStoreItem;
 end
 g_mpLoadingScreen.loadFunction = Utils.prependedFunction(g_mpLoadingScreen.loadFunction, WashAtConfig.initialize);
 
@@ -32,16 +32,16 @@ function WashAtConfig:load(missionInfo, missionDynamicInfo, loadingScreen)
     self:print("load()");
     g_currentMission.loadMapFinished = Utils.appendedFunction(g_currentMission.loadMapFinished, self.loadMapFinished);
     g_currentMission.onStartMission = Utils.appendedFunction(g_currentMission.onStartMission, self.afterLoad);
-    g_currentMission.missionInfo.saveToXML = Utils.appendedFunction(g_currentMission.missionInfo.saveToXML, self.saveSavegame);
+    g_currentMission.onObjectCreated = Utils.appendedFunction(g_currentMission.onObjectCreated, WashAtConfig.onObjectCreated);
+    g_currentMission.onObjectDeleted = Utils.appendedFunction(g_currentMission.onObjectDeleted, WashAtConfig.onObjectDeleted);
 end
 g_mpLoadingScreen.loadFunction = Utils.appendedFunction(g_mpLoadingScreen.loadFunction, WashAtConfig.load);
 
 function WashAtConfig:loadMap(name)
     self:print(("loadMap(name:%s)"):format(name));
     if self.debug then
-        --addConsoleCommand("AAAWashAtConfigeTestCommand", "", "TestCommand", self);
+        addConsoleCommand("AAAWashAtConfigeTestCommand", "", "TestCommand", self);
     end
-    self:loadSavegame();
 end
 
 function WashAtConfig:loadMapFinished()
@@ -54,22 +54,13 @@ function WashAtConfig:afterLoad()
     self:print("afterLoad");
 end
 
-function WashAtConfig:loadSavegame()
-    self:print("loadSavegame()");
-end
-
-function WashAtConfig:saveSavegame()
-    self = WashAtConfig;
-    self:print("saveSavegame()");
-end
-
 function WashAtConfig:deleteMap()
     self:print("deleteMap()");
 end
 
---function WashAtConfig:TestCommand()
---    return "This is a test command";
---end
+function WashAtConfig:TestCommand()
+    DebugUtil.printTableRecursively(self.lastObject, "", 0, 5);
+end
 
 function WashAtConfig:keyEvent(unicode, sym, modifier, isDown)
 end
@@ -83,12 +74,36 @@ end
 function WashAtConfig:draw()
 end
 
-function WashAtConfig.changeVehicleConfigRun(event, connection)
+function WashAtConfig:onObjectCreated(object)
+    local isWasher = object.washerParticleSystems ~= nil;
+    if isWasher then
+        WashAtConfig.washers = WashAtConfig.washers + 1;
+    end
+end
+
+function WashAtConfig:onObjectDeleted(object)
+    local isWasher = object.washerParticleSystems ~= nil;
+    WashAtConfig.washers = WashAtConfig.washers - 1;
+end
+
+function WashAtConfig:setStoreItem(storeItem, vehicle, configBasePrice)
+    self.storeItem = storeItem;
+    self.vehicle = vehicle;
+    configBasePrice = Utils.getNoNil(configBasePrice, 0);
+    if configBasePrice == 0 then
+        if WashAtConfig.washers <= 0 then
+            configBasePrice = 250;
+        end
+    end
+    self.configBasePrice = configBasePrice;
+end
+
+function WashAtConfig:changeVehicleConfigRun(connection)
     if not connection:getIsServer() then
-        if event.vehicle.setDirtAmount ~= nil then
-            event.vehicle:setDirtAmount(0, true);
+        if self.vehicle.setDirtAmount ~= nil then
+            self.vehicle:setDirtAmount(0, true);
         end
     end
 end
 
-addModEventListener(WashAtConfig)
+addModEventListener(WashAtConfig);
