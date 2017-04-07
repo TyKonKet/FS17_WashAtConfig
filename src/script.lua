a192ae18f05cb1a7a33ac30b7eabaa75b3472366
Matteo Autieri <matteoautieri@live.it>
2017-04-07 12:43:03 +0200

    removed useless code

--- src/script.lua
+++ src/script.lua
@@ -8,20 +8,8 @@ WashAtConfig.name = "WashAtConfig";
 WashAtConfig.debug = false;
 WashAtConfig.washers = 0;
 
-function WashAtConfig:print(txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9)
-    if self.debug then
-        local args = {txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9};
-        for i, v in ipairs(args) do
-            if v then
-                print("[" .. self.name .. "] -> " .. tostring(v));
-            end
-        end
-    end
-end
-
 function WashAtConfig:initialize(missionInfo, missionDynamicInfo, loadingScreen)
     self = WashAtConfig;
-    self:print("initialize()");
     ChangeVehicleConfigEvent.run = Utils.prependedFunction(ChangeVehicleConfigEvent.run, WashAtConfig.changeVehicleConfigRun);
     g_shopConfigScreen.setStoreItem = WashAtConfig.setStoreItem;
 end
@@ -29,7 +17,6 @@ g_mpLoadingScreen.loadFunction = Utils.prependedFunction(g_mpLoadingScreen.loadF
 
 function WashAtConfig:load(missionInfo, missionDynamicInfo, loadingScreen)
     self = WashAtConfig;
-    self:print("load()");
     g_currentMission.loadMapFinished = Utils.appendedFunction(g_currentMission.loadMapFinished, self.loadMapFinished);
     g_currentMission.onStartMission = Utils.appendedFunction(g_currentMission.onStartMission, self.afterLoad);
     g_currentMission.onObjectCreated = Utils.appendedFunction(g_currentMission.onObjectCreated, WashAtConfig.onObjectCreated);
@@ -38,28 +25,17 @@ end
 g_mpLoadingScreen.loadFunction = Utils.appendedFunction(g_mpLoadingScreen.loadFunction, WashAtConfig.load);
 
 function WashAtConfig:loadMap(name)
-    self:print(("loadMap(name:%s)"):format(name));
-    if self.debug then
-        addConsoleCommand("AAAWashAtConfigeTestCommand", "", "TestCommand", self);
-    end
 end
 
 function WashAtConfig:loadMapFinished()
     self = WashAtConfig;
-    self:print("loadMapFinished()");
 end
 
 function WashAtConfig:afterLoad()
     self = WashAtConfig;
-    self:print("afterLoad");
 end
 
 function WashAtConfig:deleteMap()
-    self:print("deleteMap()");
-end
-
-function WashAtConfig:TestCommand()
-    DebugUtil.printTableRecursively(self.lastObject, "", 0, 5);
 end
 
 function WashAtConfig:keyEvent(unicode, sym, modifier, isDown)
